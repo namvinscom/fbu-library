25f212b4c6889e89f07d746126175666f4518dd7
Nguyễn Hải Nam <nam75417@gmail.com>
2025-12-22 23:02:52 +0700

    Add fix link route11231323123

--- resources/views/page/QuanLySach.blade.php
+++ resources/views/page/QuanLySach.blade.php
@@ -149,7 +149,7 @@
         </div>
         <p class="mb-4">Bạn có chắc chắn muốn xóa sách này không?</p>
         
-        <form method="POST" action="{{ route('book.delete') }}">
+        <form id="deleteBookModal" method="POST" action="{{ route('book.delete') }}">
             @csrf
             <input type="hidden" name="id" id="delete_book_id">
 
